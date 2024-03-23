96d71c3a8e141d5d8bc59f9ca041aabf6d3f059d
Nguyen Van Trung <trungq108@gmail.com>
2024-03-23 23:00:20 +0700

    23-3 nu

--- Assets/MovementTest.cs
+++ Assets/MovementTest.cs
@@ -8,11 +8,23 @@ public class MovementTest : MonoBehaviour
 
     private void Update()
     {
-       
+        Move();
     }
 
     void Move()
     {
-        transform.position += transform.up * speed * Time.deltaTime;
+        Transform camTransform = Camera.main.transform;
+
+        Vector3 camPosition = new Vector3(
+    camTransform.position.x,
+    transform.position.y,
+    camTransform.position.z);
+        Vector3 direction = (transform.position - camPosition).normalized;
+
+        Vector3 forwardMovement = direction * Input.GetAxis("Vertical");
+        Vector3 horizontalMovement = camTransform.right * Input.GetAxis("Horizontal");
+
+        Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement, 1);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
