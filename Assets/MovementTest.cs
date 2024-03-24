8f5c2feb40cef31e778e1ffb769ede9f3ff2519f
Nguyen Van Trung <trungq108@gmail.com>
2024-03-24 13:31:15 +0700

    24-3 day

--- Assets/MovementTest.cs
+++ Assets/MovementTest.cs
@@ -1,30 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementTest : MonoBehaviour
 {
-    public float speed = 2f;
+    [SerializeField] float t;
 
     private void Update()
     {
-        Move();
-    }
-
-    void Move()
-    {
-        Transform camTransform = Camera.main.transform;
-
-        Vector3 camPosition = new Vector3(
-    camTransform.position.x,
-    transform.position.y,
-    camTransform.position.z);
-        Vector3 direction = (transform.position - camPosition).normalized;
-
-        Vector3 forwardMovement = direction * Input.GetAxis("Vertical");
-        Vector3 horizontalMovement = camTransform.right * Input.GetAxis("Horizontal");
-
-        Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement, 1);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        float lept = Mathf.Lerp(0, 3000, t);
+        Debug.Log(lept);
     }
 }
